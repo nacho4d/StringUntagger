5f0858cb88f8263c868c4824915531df0105b8b3
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2020-04-23 15:41:32 +0900

    Add gitlab-ci.yml

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/nacho4d/XCTestHTMLReport", .branch("xcode11_4_and_skipped_and_junit"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,6 +24,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "StringTagProcessorTests",
-            dependencies: ["StringTagProcessor"]),
+            dependencies: ["StringTagProcessor", "xchtmlreport"]),
     ]
 )
