47b34378818a0d53ba42848f6f5520a16dd2c1a4
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2020-05-18 03:28:38 +0900

    Remove xchtmlreport dependency

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/nacho4d/XCTestHTMLReport", .branch("xcode11_4_and_skipped_and_junit"))
+        //.package(url: "https://github.com/nacho4d/XCTestHTMLReport", .branch("xcode11_4_and_skipped_and_junit"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +24,9 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "StringUntaggerTests",
-            dependencies: ["StringUntagger", "xchtmlreport"]),
+            dependencies: [
+                "StringUntagger",
+                //"xchtmlreport"
+        ]),
     ]
 )
